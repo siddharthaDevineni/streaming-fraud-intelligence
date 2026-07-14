5218f9619abb7133dd6a365d43788c6ce269a0d9
siddharthaDevineni <siddhartha.devineni@gmail.com>
2026-07-14 19:31:19 +0200

    CHG: use docker env variables

--- src/test/java/com/agenticfraud/engine/testing/TestDataGenerator.java
+++ src/test/java/com/agenticfraud/engine/testing/TestDataGenerator.java
@@ -61,7 +61,8 @@ public class TestDataGenerator {
 
   private static KafkaProducer<String, String> createProducer() {
     Properties props = new Properties();
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+            System.getenv().getOrDefault("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092"));
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     props.put(ProducerConfig.ACKS_CONFIG, "all");
